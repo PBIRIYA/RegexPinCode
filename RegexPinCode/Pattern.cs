560690e54470ae39f6437e72ea499ea36b4034c5
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-07 14:59:24 +0530

    [Priya] add . made pin

---
+++ RegexPinCode/Pattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace RegexPinCode
+{
+    class Pattern
+    {
+        //aaaaa, sdfg45// pincode -> 6digi, 
+        string Regex_PinCode = ("^[1-9]{5}$");
+        public bool validatePinCode(string pincode)
+            {
+            return Regex.IsMatch(pincode, Regex_PinCode);
+        }
+    }
+}
+
---
+++ RegexPinCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Dynamic;
+
+namespace RegexPinCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern patterns = new Pattern();
+            bool result = patterns.validatePinCode("1234567");
+            Console.WriteLine(result);
+        }
+    }
+}
